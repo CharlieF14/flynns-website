cda0d3ca78935e4f0f533da71fd3d16373d0f439
CharlieF14 <112865083+CharlieF14@users.noreply.github.com>
2022-09-22 07:46:26 +0100

    Add files via upload

---
+++ Introduction to Javascript.js
@@ -0,0 +1,45 @@
+const PI = 3.14;
+
+//Q1
+let r = 5;
+var A = PI*(r**2);
+console.log(A);
+
+var C = 2*PI*r;
+console.log(C);
+
+//Q2
+let l = 7;
+let w = 8;
+let A2 = l*w;
+console.log(A2);
+
+//Q3
+let b = 14;
+let h = 5;
+var A3 = (1/2)*b*h;
+console.log(A3);
+
+//Q4
+//let c = 6;
+let a = 4;
+let b2 = 3;
+var c = Math.sqrt(a**2 + b**2);
+console.log(c);
+
+//Q5
+let l2 = 5;
+let w2 = 9;
+let h2 = 3;
+var V = l2*w2*h2;
+
+//Q6
+let r2 = 4;
+let h3 = 2;
+var V2 = PI*(r2**2)*h3;
+console.log(V2);
+
+//Q6
+let r3 = 14;
+var V3 = (4/3)*PI*(r3**3);
+console.log(V3);
